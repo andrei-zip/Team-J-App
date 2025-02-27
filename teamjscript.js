6e6857d3ead2e9498a933c8d6a99a126b5404e07
amoraru <amoraru@uclan.ac.uk>
2025-02-27 15:52:25 +0200

    changed the button for hunt, added new page for the list / hunt.

---
+++ teamjscript.js
@@ -0,0 +1,9 @@
+async function letsHunt(){
+    const response = await
+        fetch('https://codecyprus.org/th/api/list');
+
+    const reply = await response.text();
+
+    document.getElementById("myDiv").innerHTML = reply;
+}
+letsHunt();
