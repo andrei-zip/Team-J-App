05111678fda59a91d08b8d1ec0b11494aad918fc
Cryabyss <rommysgoog@gmail.com>
2025-03-09 15:10:20 +0200

    Fix some bugs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
-const API_URL = "https://codecyprus.org/th";
+const API_URL = 'https://codecyprus.org/th/api/';
 
 let score = 0;
 let currentQuestion = null;
 let timer = 1800; // 30 minutes per seconds
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetchQuestion();
+    setInterval(updateTimer, 1000);
+    document.getElementById("submit-answer").addEventListener("click", submitAnswer);
+    document.getElementById("get-location").addEventListener("click", updateLocation);
+});
